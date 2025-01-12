04d81511cfdd83b8dcfad939dcc03fe94ecb3875
NAEfimov <n.a.efimov@gmail.com>
2025-01-12 14:07:24 +0100

    add cmd_lst.c with cmd list operators

---
+++ cmd_lst.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_lst.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nefimov <nefimov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 10:40:57 by nefimov           #+#    #+#             */
+/*   Updated: 2025/01/12 14:02:03 by nefimov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+#include "libft.h"
+
+// Add a command to the list of commands
+// Return <0> if ok, <-1> if malloc error
+int	add_cmd(t_list **cmd_lst, int lst, int num)
+{
+	int		*cmd;
+	t_list	*node;
+	
+	cmd = (int *) malloc(sizeof(int) * 2);
+	if (cmd == NULL)
+		return (-1);
+	cmd[0] = lst;
+	cmd[1] = num;
+	node = ft_lstnew(cmd);
+	if (node == NULL)
+		return (-2);
+	ft_lstadd_front(cmd_lst, node);
+	return (0);
+}
+
+// Read the first command in the list of commands
+int	*read_cmd(t_list *cmd_lst)
+{
+	int 	*cmd;
+
+	if (cmd_lst)
+		cmd = (int *) cmd_lst->content;
+	else
+		cmd = NULL;
+	return (cmd);
+}
+
+// Read the first command in the list of commands
+// and remove it from the list
+int	*get_cmd(t_list **cmd_lst)
+{
+	t_list	*node;
+	int		*cmd;
+
+	cmd = read_cmd(*cmd_lst);
+	if (*cmd_lst)
+	{
+		node = *cmd_lst;
+		*cmd_lst = (*cmd_lst)->next;
+		ft_lstdelone(node, del_none);
+	}
+	return (cmd);
+}
+
+// Initialise the list of commands
+// Return <0> if ok, <-1> if error
+int	init_cmd(t_list **cmd_lst, int len)
+{
+	return (add_cmd(cmd_lst, A, len));
+}
+
